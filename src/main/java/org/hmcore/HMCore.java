03e0cebc461e538ba1c733a0fda24c120c8a2fa6
Wieland Schöbl <wulkanat@gmail.com>
2021-04-11 22:59:23 +0200

    Make modules final

--- src/main/java/org/hmcore/HMCore.java
+++ src/main/java/org/hmcore/HMCore.java
@@ -6,7 +6,7 @@ import java.util.HashMap;
 
 public class HMCore {
 
-    public static HashMap<String, Module<?, ?>> modules = new HashMap<>();
+    public final static HashMap<String, Module<?, ?>> modules = new HashMap<>();
 
     public static void callAllModuleRegistries() {
         modules.forEach((name, module) -> module.registerObjects());
