7be51851e98c719f0fe0d75ba4ef0b4f9d722172
Wieland Schöbl <wulkanat@gmail.com>
2021-04-11 23:09:28 +0200

    Fix Kotlin tests

--- src/test/kotlin/org/hmcore/tests/KotlinTests.kt
+++ src/test/kotlin/org/hmcore/tests/KotlinTests.kt
@@ -6,12 +6,14 @@ import org.junit.jupiter.api.Test
 // The primary function of the Kotlin tests is to ensure Kotlin-specific features are preserved
 // Meaning some tests might only make sure features work at compile time.
 
-@Test
-fun `Module should support index access operator`() {
-    HMCore.modules["ores"]!!["other"]
-}
+class KotlinTests {
+    @Test
+    fun `Module should support index access operator`() {
+        HMCore.modules["ores"]?.apply { this["other"] }
+    }
 
-@Test
-fun `Module should support 'in' keyword`() {
-    HMCore.modules["ores"]?.let { "copper" in it }
+    @Test
+    fun `Module should support 'in' keyword`() {
+        HMCore.modules["ores"]?.let { "copper" in it }
+    }
 }
